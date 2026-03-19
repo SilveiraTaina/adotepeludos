59df7caf3d08a9d054f3bce9ad77987bea6bcce4
Tainá Silveira <taina.o.silveira@gmail.com>
2026-03-19 14:26:30 -0300

    ajustando front da tela de login

--- adotepeludos/app/login.tsx
+++ adotepeludos/app/login.tsx
@@ -1,17 +1,20 @@
-// app/login.tsx
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { router } from 'expo-router';
-
 export default function LoginScreen() {
   const handleLogin = () => {
-    // Aqui você vai adicionar a lógica de autenticação depois
-    // Por enquanto, só navega para as tabs
+    // adicionar a lógica de autenticação depois
     router.replace('/(tabs)');
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Bem-vinda ao Adote Peludos</Text>
+  <Image 
+    source={require('../assets/images/logo.png')} 
+    style={styles.logo}
+    resizeMode="contain"
+  />
+  <Text style={styles.mainTitle}>Adote Peludos</Text>
+  <Text style={styles.subtitle}>Capão da Canoa</Text>
       
       <TextInput 
         placeholder="E-mail" 
@@ -26,7 +29,9 @@ export default function LoginScreen() {
         style={styles.input}
       />
       
-      <Button title="Entrar" onPress={handleLogin} />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
       
       <Text style={styles.link}>Não tem conta? Cadastre-se</Text>
     </View>
@@ -40,23 +45,53 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#fff'
   },
-  title: { 
-    fontSize: 24, 
-    marginBottom: 30, 
+  logo: {
+    width: '100%',
+    height: 120,
+    marginBottom: 20,
+    alignSelf: 'center'
+  },
+  mainTitle: { 
+    fontSize: 32,
+    marginBottom: 8,
     textAlign: 'center',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    color: '#FF6B00' 
+  },
+  subtitle: { 
+    fontSize: 18,
+    marginBottom: 40,
+    textAlign: 'center',
+    color: '#FFA500'
   },
   input: { 
     borderWidth: 1,
-    borderColor: '#ddd',
+    borderColor: '#FFA500', 
     padding: 12, 
     marginBottom: 15, 
     borderRadius: 8,
     fontSize: 16
   },
+   button: {
+    backgroundColor: '#FF6B00',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
   link: {
     marginTop: 20,
     textAlign: 'center',
-    color: '#007AFF'
+    color: '#FF6B00'
   } 
-  });
+});
